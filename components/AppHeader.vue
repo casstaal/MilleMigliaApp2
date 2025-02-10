47284760e5605977972e0662a0b2c2887fd35e76
casstaal <casstaal@hotmail.com>
2025-02-10 19:25:48 +0100

    Added logout button

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,7 +1,18 @@
+<script setup lang="ts">
+    const { signOut } = useAuth();
+
+    const logout = () => {
+        signOut();
+    }
+
+</script>
+
+
 <template>
     <div class="header d-flex justify-content-start align-items-center">
         <img src="/MilleMigliaLogo.png" alt="Mille Miglia logo">
-        <h1 class="d-flex align-items-center justify-content-center mx-auto">Mille Miglia</h1>
+        <!-- <h1 class="d-flex align-items-center justify-content-center mx-auto">Mille Miglia</h1> -->
+        <button class="btn-logout btn btn-primary" @click="logout">Logout</button>
     </div>
 </template>
 
--- server/api/checks/index.post.ts
+++ server/api/checks/index.post.ts
@@ -1,12 +1,12 @@
 import { getServerSession } from "#auth";
 
 export default defineEventHandler(async (event) => {
-    // const session = await getServerSession(event);
+    const session = await getServerSession(event);
 
-    // if(!session) {
-    //     sendError(event, createError({ statusMessage: "Unauthenticated", statusCode: 401}));
-    //     return;
-    // }
+    if(!session) {
+        sendError(event, createError({ statusMessage: "Unauthenticated", statusCode: 401}));
+        return;
+    }
 
     const prisma = usePrisma();
     const body = await readBody(event);
