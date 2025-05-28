85e8cb1ac760554769f46b7852bed3e0935dfc45
casstaal <casstaal@hotmail.com>
2025-05-28 20:24:49 +0200

    Fixed user role on account page.

--- pages/account.vue
+++ pages/account.vue
@@ -15,6 +15,8 @@
     const route = useRoute();
     const router = useRouter();
 
+    const loggedInUser = await useFetch<User>(`/api/users/${session?.user?.userId}`)
+
     const users = ref<User[] | null>(null);
     const { data: userData } = await useFetch<User[]>(`/api/users`);
     users.value = userData.value;
@@ -262,7 +264,7 @@
                 <h2>Statistieken</h2>
                 <!-- Hoeveel posts toegevoegd, hoeveel markers toegevoegd en dan laten zien wat het percentage is van de totaal toegevoegd posts en markers -->
                 <div class="row">
-                    <div class="col-lg-3 col-12">
+                    <div class="col-lg-3 col-12 mb-lg-0 mb-3">
                         <canvas ref="chartCanvas" width="400" height="400"></canvas>
                     </div>
                     <div class="col-lg-3 col-12">
@@ -272,8 +274,8 @@
             </div>
         </div>
 
-        <div v-if="session?.user?.role === 'admin'" class="row">
-            <div class="col-12 ps-lg-5 pe-lg-0 ps-4 pe-4">
+        <div v-if="loggedInUser?.data.value?.role !== 'admin'" class="row">
+            <div class="col-12 ps-lg-5 pe-lg-0 ps-4 pe-4 mb-5 mt-3">
                 <h2 class="text-white">Reset password</h2>
                 <form @submit="onUpdateSubmit" class="reset-password-form border rounded bg-white p-3 col-lg-3">
                     <div class="mt-2">
@@ -294,117 +296,119 @@
         </div>
 
         <!-- <div v-if="session?.user?.role === 'admin'" class="row mt-5"> -->
-        <div class="row mt-5">
-            <div class="col-lg-4 pe-lg-3 ps-lg-5 ps-4 pe-4 mb-4">
-                <div class="d-flex align-items-center gap-2">
-                    <h2 class="mb-0 text-white">Users</h2>
-                    <div v-if="isSelected">
-                        <Icon
-                        icon="codicon:trash"
-                        :style="{ fontSize: '26px', cursor: 'pointer', color: '#FF0000' }"
-                        :ssr="true"
-                        @click="deleteUser()"
-                        />
-                    </div>
-                </div>
-
-
-                <!-- laat zien of een user nu actief is of wanneer de laatste activiteit is. Hoeveel markers een user heeft toegevoegd en wanneer de laatste toevoeging is. -->
-                <!-- Kleine dropdown om extra info van user te laten zien. Als hij ingeklapt is alleen de naam en groen puntje of actief is en anders hoelang geleden hij actief was. -->
-                <!-- delete voor user (select user en dan verschijnt er een delete knop) -->
-                <div class="overflow-auto" style="max-height: 450px;">
-                    <div v-if="userUsers?.length === 0" class="text-center text-muted p-3">
-                        Geen gebruikers gevonden
-                    </div>
-                    <div v-for="user in userUsers" :key="user.id" class="card p-3 shadow-sm mb-3 bg-white" :class="{ 'selected': selectedUserId === user.id }" @click="toggleSelectMode(user.id)">
-                        <div class="card-body">
-                                <div class="row">
-                                    <div class="col-4 d-flex flex-column justify-content-center">
-                                        <p class="mb-1">
-                                            <strong>{{ user.name }}</strong>
-                                        </p>
-                                        <p class="text-muted mb-0">{{ user.email }}</p>
-                                    </div>
-                                    <div class="col-4 d-flex align-items-center justify-content-center">
-                                        <p class="me-2 mb-0">Markers: 12</p>
-                                        <p class="me-2 mb-0">Posts: 5</p>
-                                    </div>
-                                    <div class="col-4 d-flex align-items-center justify-content-end">
-                                        <p class="me-2 mb-0">Laatst online: 5 days ago</p>
-                                    </div>
-                                </div>
+        <div v-if="loggedInUser?.data?.value?.role === 'admin'">
+            <div class="row mt-5">
+                <div class="col-lg-4 pe-lg-3 ps-lg-5 ps-4 pe-4 mb-4">
+                    <div class="d-flex align-items-center gap-2">
+                        <h2 class="mb-0 text-white">Users</h2>
+                        <div v-if="isSelected">
+                            <Icon
+                            icon="codicon:trash"
+                            :style="{ fontSize: '26px', cursor: 'pointer', color: '#FF0000' }"
+                            :ssr="true"
+                            @click="deleteUser()"
+                            />
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="col-lg-4 pe-lg-3 ps-lg-5 ps-4 pe-4 mb-4">
-                <div class="d-flex align-items-center gap-2">
-                    <h2 class="mb-0 text-white">Guests</h2>
-                    <div v-if="isSelected">
-                        <Icon
-                        icon="codicon:trash"
-                        :style="{ fontSize: '26px', cursor: 'pointer', color: '#FF0000' }"
-                        :ssr="true"
-                        @click="deleteUser()"
-                        />
-                    </div>
-                </div>
 
 
-                <!-- laat zien of een user nu actief is of wanneer de laatste activiteit is. Hoeveel markers een user heeft toegevoegd en wanneer de laatste toevoeging is. -->
-                <!-- Kleine dropdown om extra info van user te laten zien. Als hij ingeklapt is alleen de naam en groen puntje of actief is en anders hoelang geleden hij actief was. -->
-                <!-- delete voor user (select user en dan verschijnt er een delete knop) -->
-                <div class="overflow-auto" style="max-height: 450px;">
-                    <div v-if="guestUsers?.length === 0" class="text-center text-muted p-3">
-                        Geen gasten gevonden
-                    </div>
-                    <div v-for="user in guestUsers" :key="user.id" class="card p-3 shadow-sm mb-3 bg-white" :class="{ 'selected': selectedUserId === user.id }" @click="toggleSelectMode(user.id)">
-                        <div class="card-body">
-                                <div class="row">
-                                    <div class="col-4 d-flex flex-column justify-content-center">
-                                        <p class="mb-1">
-                                            <strong>{{ user.name }}</strong>
-                                        </p>
-                                        <p class="text-muted mb-0">{{ user.email }}</p>
+                    <!-- laat zien of een user nu actief is of wanneer de laatste activiteit is. Hoeveel markers een user heeft toegevoegd en wanneer de laatste toevoeging is. -->
+                    <!-- Kleine dropdown om extra info van user te laten zien. Als hij ingeklapt is alleen de naam en groen puntje of actief is en anders hoelang geleden hij actief was. -->
+                    <!-- delete voor user (select user en dan verschijnt er een delete knop) -->
+                    <div class="overflow-auto" style="max-height: 450px;">
+                        <div v-if="userUsers?.length === 0" class="text-center text-muted p-3">
+                            Geen gebruikers gevonden
+                        </div>
+                        <div v-for="user in userUsers" :key="user.id" class="card p-3 shadow-sm mb-3 bg-white" :class="{ 'selected': selectedUserId === user.id }" @click="toggleSelectMode(user.id)">
+                            <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-4 d-flex flex-column justify-content-center">
+                                            <p class="mb-1">
+                                                <strong>{{ user.name }}</strong>
+                                            </p>
+                                            <p class="text-muted mb-0">{{ user.email }}</p>
+                                        </div>
+                                        <div class="col-4 d-flex align-items-center justify-content-center">
+                                            <p class="me-2 mb-0">Markers: 12</p>
+                                            <p class="me-2 mb-0">Posts: 5</p>
+                                        </div>
+                                        <div class="col-4 d-flex align-items-center justify-content-end">
+                                            <p class="me-2 mb-0">Laatst online: 5 days ago</p>
+                                        </div>
                                     </div>
-                                    <div class="col-4 d-flex align-items-center justify-content-center">
-                                        <p class="me-2 mb-0">Markers: 12</p>
-                                        <p class="me-2 mb-0">Posts: 5</p>
-                                    </div>
-                                    <div class="col-4 d-flex align-items-center justify-content-end">
-                                        <p class="me-2 mb-0">Laatst online: 5 days ago</p>
-                                    </div>
-                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-4 ps-lg-3 pe-lg-5 ps-4 pe-4 mb-5">
-                <h2 class="text-white">Add new user</h2>
-                <form  @submit="onSubmit" class="addUser-form border rounded bg-white">
-                    <div class="ms-4 me-4">
-                        <div class="mt-3">
-                            <label for="name" class="form-label fs-5">Name:</label>
-                            <input class="form-control input-lg" :class="{ 'is-invalid': addUserErrors.name}" id="name" type="text" v-model="name" placeholder="Name">
-                        </div>
-                        <div class="mt-3">
-                            <label for="email" class="form-label fs-5">Email:</label>
-                            <input class="form-control input-lg" :class="{ 'is-invalid': addUserErrors.email}" id="email" type="email" v-model="email" placeholder="Email">
-                        </div>
-                        <div class="mt-3">
-                            <label for="password" class="form-label fs-5">Password:</label>
-                            <input class="form-control input-lg" :class="{ 'is-invalid': addUserErrors.password}" id="password" type="password" v-model="password" placeholder="Password">
-                        </div>
-                        <div class="mt-3">
-                            <label for="role" class="form-label fs-5">Role:</label><br>
-                            <select class="form-control input-lg" :class="{ 'is-invalid': addUserErrors.role}" name="role" id="role" v-model="role">
-                                <option value="guest">Guest</option>
-                                <option value="user">User</option>
-                            </select>
+                <div class="col-lg-4 pe-lg-3 ps-lg-5 ps-4 pe-4 mb-4">
+                    <div class="d-flex align-items-center gap-2">
+                        <h2 class="mb-0 text-white">Guests</h2>
+                        <div v-if="isSelected">
+                            <Icon
+                            icon="codicon:trash"
+                            :style="{ fontSize: '26px', cursor: 'pointer', color: '#FF0000' }"
+                            :ssr="true"
+                            @click="deleteUser()"
+                            />
                         </div>
                     </div>
-                    <button class="create-button btn btn-lg mt-3 mb-3 me-5 ms-4 col-12" type="submit">Save user</button>
-                </form>
+
+
+                    <!-- laat zien of een user nu actief is of wanneer de laatste activiteit is. Hoeveel markers een user heeft toegevoegd en wanneer de laatste toevoeging is. -->
+                    <!-- Kleine dropdown om extra info van user te laten zien. Als hij ingeklapt is alleen de naam en groen puntje of actief is en anders hoelang geleden hij actief was. -->
+                    <!-- delete voor user (select user en dan verschijnt er een delete knop) -->
+                    <div class="overflow-auto" style="max-height: 450px;">
+                        <div v-if="guestUsers?.length === 0" class="text-center text-muted p-3">
+                            Geen gasten gevonden
+                        </div>
+                        <div v-for="user in guestUsers" :key="user.id" class="card p-3 shadow-sm mb-3 bg-white" :class="{ 'selected': selectedUserId === user.id }" @click="toggleSelectMode(user.id)">
+                            <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-4 d-flex flex-column justify-content-center">
+                                            <p class="mb-1">
+                                                <strong>{{ user.name }}</strong>
+                                            </p>
+                                            <p class="text-muted mb-0">{{ user.email }}</p>
+                                        </div>
+                                        <div class="col-4 d-flex align-items-center justify-content-center">
+                                            <p class="me-2 mb-0">Markers: 12</p>
+                                            <p class="me-2 mb-0">Posts: 5</p>
+                                        </div>
+                                        <div class="col-4 d-flex align-items-center justify-content-end">
+                                            <p class="me-2 mb-0">Laatst online: 5 days ago</p>
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 ps-lg-3 pe-lg-5 ps-4 pe-4 mb-5">
+                    <h2 class="text-white">Add new user</h2>
+                    <form  @submit="onSubmit" class="addUser-form border rounded bg-white">
+                        <div class="ms-4 me-4">
+                            <div class="mt-3">
+                                <label for="name" class="form-label fs-5">Name:</label>
+                                <input class="form-control input-lg" :class="{ 'is-invalid': addUserErrors.name}" id="name" type="text" v-model="name" placeholder="Name">
+                            </div>
+                            <div class="mt-3">
+                                <label for="email" class="form-label fs-5">Email:</label>
+                                <input class="form-control input-lg" :class="{ 'is-invalid': addUserErrors.email}" id="email" type="email" v-model="email" placeholder="Email">
+                            </div>
+                            <div class="mt-3">
+                                <label for="password" class="form-label fs-5">Password:</label>
+                                <input class="form-control input-lg" :class="{ 'is-invalid': addUserErrors.password}" id="password" type="password" v-model="password" placeholder="Password">
+                            </div>
+                            <div class="mt-3">
+                                <label for="role" class="form-label fs-5">Role:</label><br>
+                                <select class="form-control input-lg" :class="{ 'is-invalid': addUserErrors.role}" name="role" id="role" v-model="role">
+                                    <option value="guest">Guest</option>
+                                    <option value="user">User</option>
+                                </select>
+                            </div>
+                        </div>
+                        <button class="create-button btn btn-lg mt-3 mb-3 me-5 ms-4 col-12" type="submit">Save user</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
